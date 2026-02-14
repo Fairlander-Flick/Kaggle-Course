90dcf7d1efc9bc8fa2bed8c369ee64e288714187
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-14 13:26:15 +0100

    Update Working with External Libraries exercise content

--- Python/Working with External Libraries.py
+++ Python/Working with External Libraries.py
@@ -2,3 +2,98 @@
 #  Kaggle - Python
 #  Exercise: Working with External Libraries
 # ================================================================
+
+
+# ----------------------------------------------------------------
+#  Q1 - Jimmy's Slot Machine Graph
+# ----------------------------------------------------------------
+
+# Import the jimmy_slots submodule
+from learntools.python import jimmy_slots
+# Call the get_graph() function to get Jimmy's graph
+graph = jimmy_slots.get_graph()
+graph
+
+
+def prettify_graph(graph):
+    """Modify the given graph according to Jimmy's requests: add a title, make the y-axis
+    start at 0, label the y-axis. (And, if you're feeling ambitious, format the tick marks
+    as dollar amounts using the "$" symbol.)
+    """
+    graph.set_title("Results of 500 slot machine pulls")
+    # Complete steps 2 and 3 here
+    graph.set_ylim(0)
+    graph.set_ylabel("Balance")
+
+graph = jimmy_slots.get_graph()
+prettify_graph(graph)
+graph
+
+
+# ----------------------------------------------------------------
+#  Q2 - Luigi's Race Data
+# ----------------------------------------------------------------
+
+# Import luigi's full dataset of race data
+from learntools.python.luigi_analysis import full_dataset
+
+# Fix me!
+def best_items(racers):
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for item in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if item not in winner_item_counts:
+                    winner_item_counts[item] = 0
+                winner_item_counts[item] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(i+1, len(racers), racer['name']))
+    return winner_item_counts
+
+# Try analyzing the imported full dataset
+best_items(full_dataset)
+
+
+# ----------------------------------------------------------------
+#  Q3 - Blackjack Hand Comparison
+# ----------------------------------------------------------------
+
+def hand_total(hand):
+    """Helper function to calculate the total points of a blackjack hand.
+    """
+    total = 0
+    # Count the number of aces and deal with how to apply them at the end.
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            aces += 1
+        else:
+            # Convert number cards (e.g. '7') to ints
+            total += int(card)
+    # At this point, total is the sum of this hand's cards *not counting aces*.
+
+    # Add aces, counting them as 1 for now. This is the smallest total we can make from this hand
+    total += aces
+    # "Upgrade" aces from 1 to 11 as long as it helps us get closer to 21
+    # without busting
+    while total + 10 <= 21 and aces > 0:
+        # Upgrade an ace from 1 to 11
+        total += 10
+        aces -= 1
+    return total
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    total_1 = hand_total(hand_1)
+    total_2 = hand_total(hand_2)
+    return total_1 <= 21 and (total_1 > total_2 or total_2 > 21)
+
+# Check your answer
+q3.check()

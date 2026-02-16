ed88aecabe5010bd33377189c33469c6b506f914
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-16 09:59:26 +0100

    Update Data Leakage.py Q5 solution

---
+++ Intermediate Machine Learning/Data Leakage.py
@@ -0,0 +1,12 @@
+# ================================================================
+#  Kaggle - Intermediate Machine Learning
+#  Exercise: Data Leakage
+# ================================================================
+
+# Q5
+
+# Fill in the line below with one of 1, 2, 3 or 4.
+potential_leakage_feature = 2
+
+# Check your answer
+q_5.check()

1966bafe9c2eeb0db9de48f0d2019d07283bf872
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-19 05:46:18 +0100

    Update Choosing Plot Types and Custom Styles.py

--- Data Visualization/Choosing Plot Types and Custom Styles.py
+++ Data Visualization/Choosing Plot Types and Custom Styles.py
@@ -1,4 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # ================================================================
 #  Kaggle - Data Visualization
 #  Exercise: Choosing Plot Types and Custom Styles
 # ================================================================
+
+# ----------------------------------------------------------------
+#  Q1 - Line Chart
+# ----------------------------------------------------------------
+
+# Change the style of the figure
+sns.set_style("dark")
+
+# Line chart 
+plt.figure(figsize=(12,6))
+sns.lineplot(data=spotify_data)
+
+# Mark the exercise complete after the code cell is run
+step_1.check()

5583ed6e07928d33f99fcbe28b0dfc281bcd4522
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-19 08:00:25 +0100

    Update Target Encoding.py

--- Feature Engineering/Target Encoding.py
+++ Feature Engineering/Target Encoding.py
@@ -2,3 +2,84 @@
 #  Kaggle - Feature Engineering
 #  Target Encoding
 # ================================================================
+
+# ----------------------------------------------------------------
+#  Q1 - Encoding Split
+# ----------------------------------------------------------------
+
+# View the solution (Run this cell to receive credit!)
+q_1.check()
+# Encoding split
+X_encode = df.sample(frac=0.20, random_state=0)
+y_encode = X_encode.pop("SalePrice")
+
+# Training split
+X_pretrain = df.drop(X_encode.index)
+y_train = X_pretrain.pop("SalePrice")
+
+
+# ----------------------------------------------------------------
+#  Q2 - MEstimate Encoder
+# ----------------------------------------------------------------
+
+# YOUR CODE HERE: Create the MEstimateEncoder
+encoder = MEstimateEncoder(
+    cols=["Neighborhood"],
+    m=1.0,
+)
+
+# Fit the encoder on the encoding split
+encoder.fit(X_encode, y_encode)
+
+# Encode the training split
+X_train = encoder.transform(X_pretrain, y_train)
+
+# Check your answer
+q_2.check()
+
+feature = encoder.cols
+
+plt.figure(dpi=90)
+ax = sns.distplot(y_train, kde=True, hist=False)
+ax = sns.distplot(X_train[feature], color='r', ax=ax, hist=True, kde=False, norm_hist=True)
+ax.set_xlabel("SalePrice");
+
+X = df.copy()
+y = X.pop("SalePrice")
+score_base = score_dataset(X, y)
+score_new = score_dataset(X_train, y_train)
+
+print(f"Baseline Score: {score_base:.4f} RMSLE")
+print(f"Score with Encoding: {score_new:.4f} RMSLE")
+
+# Try experimenting with the smoothing parameter m
+# Try 0, 1, 5, 50
+m = 0
+
+X = df.copy()
+y = X.pop('SalePrice')
+
+# Create an uninformative feature
+X["Count"] = range(len(X))
+X["Count"][1] = 0  # actually need one duplicate value to circumvent error-checking in MEstimateEncoder
+
+# fit and transform on the same dataset
+encoder = MEstimateEncoder(cols="Count", m=m)
+X = encoder.fit_transform(X, y)
+
+# Results
+score = score_dataset(X, y)
+print(f"Score: {score:.4f} RMSLE")
+
+plt.figure(dpi=90)
+ax = sns.distplot(y, kde=True, hist=False)
+ax = sns.distplot(X["Count"], color='r', ax=ax, hist=True, kde=False, norm_hist=True)
+ax.set_xlabel("SalePrice");
+
+
+# ----------------------------------------------------------------
+#  Q3 - Overfitting with Target Encoders
+# ----------------------------------------------------------------
+
+# View the solution (Run this cell to receive credit!)
+q_3.check()

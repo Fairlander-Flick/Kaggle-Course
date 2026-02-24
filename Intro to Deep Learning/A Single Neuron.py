ab50791611254c87d86988fbe274148a9baa80ed
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-24 19:19:56 +0100

    Update Intro to Deep Learning: A Single Neuron

--- Intro to Deep Learning/A Single Neuron.py
+++ Intro to Deep Learning/A Single Neuron.py
@@ -1,2 +1,40 @@
-# A Single Neuron
-# Learn about linear units, the building blocks of deep learning.
+# ================================================================
+#  Kaggle - Intro to Deep Learning
+#  Exercise: A Single Neuron
+# ================================================================
+
+# ----------------------------------------------------------------
+#  Q1
+# ----------------------------------------------------------------
+
+input_shape = [11]
+
+# Check your answer
+q_1.check()
+
+
+# ----------------------------------------------------------------
+#  Q2
+# ----------------------------------------------------------------
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# YOUR CODE HERE
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=[11])
+])
+
+# Check your answer
+q_2.check()
+
+
+# ----------------------------------------------------------------
+#  Q3
+# ----------------------------------------------------------------
+
+w, b = model.weights
+
+print("Weights\n{}\n\nBias\n{}".format(w, b))
+# Check your answer
+q_3.check()

e2c744fdc5ecdf7ca8d2ef55494a309b0c07655d
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-24 21:53:18 +0100

    Update Intro to Deep Learning: Dropout and Batch Normalization

--- Intro to Deep Learning/Dropout and Batch Normalization.py
+++ Intro to Deep Learning/Dropout and Batch Normalization.py
@@ -1,2 +1,97 @@
-# Dropout and Batch Normalization
-# Add these special layers to prevent overfitting and stabilize training.
+# ================================================================
+#  Kaggle - Intro to Deep Learning
+#  Exercise: Dropout and Batch Normalization
+# ================================================================
+
+# ----------------------------------------------------------------
+#  Q1
+# ----------------------------------------------------------------
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# YOUR CODE HERE: Add two 30% dropout layers, one after 128 and one after 64
+model = keras.Sequential([
+    layers.Dense(128, activation='relu', input_shape=input_shape),
+    layers.Dropout(0.3),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(1)
+])
+
+# Check your answer
+q_1.check()
+
+
+# ----------------------------------------------------------------
+#  Q2
+# ----------------------------------------------------------------
+
+# View the solution (Run this cell to receive credit!)
+q_2.check()
+
+import pandas as pd
+
+concrete = pd.read_csv('../input/dl-course-data/concrete.csv')
+df = concrete.copy()
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+X_train = df_train.drop('CompressiveStrength', axis=1)
+X_valid = df_valid.drop('CompressiveStrength', axis=1)
+y_train = df_train['CompressiveStrength']
+y_valid = df_valid['CompressiveStrength']
+
+input_shape = [X_train.shape[1]]
+
+model = keras.Sequential([
+    layers.Dense(512, activation='relu', input_shape=input_shape),
+    layers.Dense(512, activation='relu'),    
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1),
+])
+model.compile(
+    optimizer='sgd', # SGD is more sensitive to differences of scale
+    loss='mae',
+    metrics=['mae'],
+)
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=64,
+    epochs=100,
+    verbose=0,
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[0:, ['loss', 'val_loss']].plot()
+print(("Minimum Validation Loss: {:0.4f}").format(history_df['val_loss'].min()))
+
+
+# ----------------------------------------------------------------
+#  Q3
+# ----------------------------------------------------------------
+
+# YOUR CODE HERE: Add a BatchNormalization layer before each Dense layer
+model = keras.Sequential([
+    layers.BatchNormalization(input_shape=input_shape),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(1),
+])
+
+# Check your answer
+q_3.check()
+
+
+# ----------------------------------------------------------------
+#  Q4
+# ----------------------------------------------------------------
+
+# View the solution (Run this cell to receive credit!)
+q_4.check()

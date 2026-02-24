a2aa3941858e2207d8c89dca2741d09d18d0fd1e
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-24 22:05:15 +0100

    Update Intro to Deep Learning: Binary Classification

--- Intro to Deep Learning/Binary Classification.py
+++ Intro to Deep Learning/Binary Classification.py
@@ -1,2 +1,47 @@
-# Binary Classification
-# Apply deep learning to another common task.
+# ================================================================
+#  Kaggle - Intro to Deep Learning
+#  Exercise: Binary Classification
+# ================================================================
+
+# ----------------------------------------------------------------
+#  Q1
+# ----------------------------------------------------------------
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+    layers.BatchNormalization(input_shape=input_shape),
+    layers.Dense(256, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(256, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(1, activation='sigmoid'),
+])
+
+# Check your answer
+q_1.check()
+
+
+# ----------------------------------------------------------------
+#  Q2
+# ----------------------------------------------------------------
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy'],
+)
+
+# Check your answer
+q_2.check()
+
+
+# ----------------------------------------------------------------
+#  Q3
+# ----------------------------------------------------------------
+
+# View the solution (Run this cell to receive credit!)
+q_3.check()

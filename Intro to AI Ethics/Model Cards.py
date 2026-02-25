9af17eaab6b21dbbffff27bc42555432f069bf8f
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-25 22:09:00 +0100

    Update Model Cards script content

--- Intro to AI Ethics/Model Cards.py
+++ Intro to AI Ethics/Model Cards.py
@@ -1,3 +1,15 @@
 # ================================================================
 #  Kaggle - Model Cards
 # ================================================================
+
+# Q1
+# Check your answer (Run this code cell to receive credit!)
+q_1.check()
+
+# Q2
+# Check your answer (Run this code cell to receive credit!)
+q_2.check()
+
+# Q3
+# Check your answer (Run this code cell to receive credit!)
+q_3.check()

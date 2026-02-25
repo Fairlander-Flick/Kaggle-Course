ed84d89fd012921d4eb2fc08ec9526fd02643192
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-25 22:00:37 +0100

    Update Identifying Bias in AI script content

--- Intro to AI Ethics/Identifying Bias in AI.py
+++ Intro to AI Ethics/Identifying Bias in AI.py
@@ -1,3 +1,40 @@
 # ================================================================
 #  Kaggle - Identifying Bias in AI
 # ================================================================
+
+# Q1
+# Comment to pass through the model
+my_comment = "I love apples and pears are worse than epstein gng"
+
+# Do not change the code below
+classify_string(my_comment)
+q_1.check()
+
+# Q2
+# Check your answer (Run this code cell to get credit!)
+q_2.check()
+
+# Q3
+# Set the value of new_comment
+new_comment = "I have a black friend and he is very smart"
+
+# Do not change the code below
+classify_string(new_comment)
+coefficients[coefficients.word.isin(new_comment.split())]
+q_3.check()
+
+# Q4
+# Check your answer (Run this code cell to get credit!)
+q_4.check()
+
+# Q5
+# Check your answer (Run this code cell to get credit!)
+q_5.check()
+
+# Q6
+# Check your answer (Run this code cell to get credit!)
+q_6.check()
+
+# Q7
+# Check your answer (Run this code cell to get credit!)
+q_7.check()

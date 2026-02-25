8e96ec73c53994335f2b06cbcfb0f27731549d3b
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-25 21:46:05 +0100

    Update Human-Centered Design for AI script content

--- Intro to AI Ethics/Human-Centered Design for AI.py
+++ Intro to AI Ethics/Human-Centered Design for AI.py
@@ -1,3 +1,27 @@
 # ================================================================
 #  Kaggle - Human-Centered Design for AI
 # ================================================================
+
+# Q1
+# Check your answer (Run this code cell to receive credit!)
+q_1.check()
+
+# Q2
+# Check your answer (Run this code cell to receive credit!)
+q_2.check()
+
+# Q3
+# Check your answer (Run this code cell to receive credit!)
+q_3.check()
+
+# Q4
+# Check your answer (Run this code cell to receive credit!)
+q_4.check()
+
+# Q5
+# Check your answer (Run this code cell to receive credit!)
+q_5.check()
+
+# Q6
+# Check your answer (Run this code cell to receive credit!)
+q_6.check()

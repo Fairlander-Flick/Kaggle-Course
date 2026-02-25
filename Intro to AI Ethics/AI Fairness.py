d5a84f0bcab0aff489eda8934dbb5791c7621fbb
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-25 22:05:13 +0100

    Update AI Fairness script content

--- Intro to AI Ethics/AI Fairness.py
+++ Intro to AI Ethics/AI Fairness.py
@@ -1,3 +1,19 @@
 # ================================================================
 #  Kaggle - AI Fairness
 # ================================================================
+
+# Q1
+# Check your answer (Run this code cell to get credit!)
+q_1.check()
+
+# Q2
+# Check your answer (Run this code cell to get credit!)
+q_2.check()
+
+# Q3
+# Check your answer (Run this code cell to get credit!)
+q_3.check()
+
+# Q4
+# Check your answer (Run this code cell to get credit!)
+q_4.check()

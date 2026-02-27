56f9122ff0cc0bc23ce3de7de0f23485a4914625
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-27 12:18:25 +0100

    Populate Scaling and Normalization script

--- Data Cleaning/Scaling and Normalization.py
+++ Data Cleaning/Scaling and Normalization.py
@@ -1,3 +1,23 @@
 # ================================================================
 #  Kaggle - Data Cleaning: Scaling and Normalization
 # ================================================================
+
+# ----------------------------------------------------------------
+# Q1
+# ----------------------------------------------------------------
+
+# select the usd_goal_real column
+original_goal_data = pd.DataFrame(kickstarters_2017.goal)
+
+# TODO: Your code here
+scaled_goal_data = minmax_scaling(original_goal_data, columns=['goal'])
+
+# Check your answer
+q1.check()
+
+# ----------------------------------------------------------------
+# Q2
+# ----------------------------------------------------------------
+
+# Check your answer (Run this code cell to receive credit!)
+q2.check()

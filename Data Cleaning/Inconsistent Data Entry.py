6447ad7595dd880b8fc2adc4e7c8972d07596192
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-27 12:27:37 +0100

    Populate Inconsistent Data Entry script

--- Data Cleaning/Inconsistent Data Entry.py
+++ Data Cleaning/Inconsistent Data Entry.py
@@ -1,3 +1,42 @@
 # ================================================================
 #  Kaggle - Data Cleaning: Inconsistent Data Entry
 # ================================================================
+
+# ----------------------------------------------------------------
+# Q1
+# ----------------------------------------------------------------
+
+# TODO: Your code here
+unis = professors['Graduated from'].unique()
+print(unis)
+
+# Check your answer (Run this code cell to receive credit!)
+q1.check()
+
+# ----------------------------------------------------------------
+# Q2
+# ----------------------------------------------------------------
+
+# TODO: Your code here
+professors['Graduated from'] = professors['Graduated from'].str.strip()
+
+# Check your answer
+q2.check()
+
+# ----------------------------------------------------------------
+# Q3
+# ----------------------------------------------------------------
+
+# get all the unique values in the 'City' column
+countries = professors['Country'].unique()
+
+# sort them alphabetically and then take a closer look
+countries.sort()
+countries
+
+# TODO: Your code here!
+matches = fuzzywuzzy.process.extract("usa", countries, limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+replace_matches_in_column(df=professors, column='Country', string_to_match="usa", min_ratio=70)
+
+# Check your answer
+q3.check()

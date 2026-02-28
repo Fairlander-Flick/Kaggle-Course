27e98d2d40f6b337b86e71a2787dea3cd7d17a05
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-02-28 11:48:45 +0100

    Populate Maximum Pooling script

--- Computer Vision/Maximum Pooling.py
+++ Computer Vision/Maximum Pooling.py
@@ -1,3 +1,76 @@
 # ================================================================
 #  Kaggle - Computer Vision: Maximum Pooling
 # ================================================================
+
+# ----------------------------------------------------------------
+# Q1
+# ----------------------------------------------------------------
+
+# Read image
+image_path = '../input/computer-vision-resources/car_illus.jpg'
+image = tf.io.read_file(image_path)
+image = tf.io.decode_jpeg(image, channels=1)
+image = tf.image.resize(image, size=[400, 400])
+
+# Embossing kernel
+kernel = tf.constant([
+    [-2, -1, 0],
+    [-1, 1, 1],
+    [0, 1, 2],
+])
+
+# Reformat for batch compatibility.
+image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+image = tf.expand_dims(image, axis=0)
+kernel = tf.reshape(kernel, [*kernel.shape, 1, 1])
+kernel = tf.cast(kernel, dtype=tf.float32)
+
+image_filter = tf.nn.conv2d(
+    input=image,
+    filters=kernel,
+    strides=1,
+    padding='VALID',
+)
+
+image_detect = tf.nn.relu(image_filter)
+
+# Show what we have so far
+plt.figure(figsize=(12, 6))
+plt.subplot(131)
+plt.imshow(tf.squeeze(image), cmap='gray')
+plt.axis('off')
+plt.title('Input')
+plt.subplot(132)
+plt.imshow(tf.squeeze(image_filter))
+plt.axis('off')
+plt.title('Filter')
+plt.subplot(133)
+plt.imshow(tf.squeeze(image_detect))
+plt.axis('off')
+plt.title('Detect')
+plt.show();
+
+# YOUR CODE HERE
+image_condense = tf.nn.pool(
+    input=image_detect,
+    window_shape=(2, 2),
+    pooling_type='MAX',
+    strides=(2, 2),
+    padding='SAME',
+)
+# Check your answer
+q_1.check()
+
+# ----------------------------------------------------------------
+# Q2
+# ----------------------------------------------------------------
+
+# View the solution (Run this code cell to receive credit!)
+q_2.solution()
+
+# ----------------------------------------------------------------
+# Q3
+# ----------------------------------------------------------------
+
+# View the solution (Run this code cell to receive credit!)
+q_3.check()

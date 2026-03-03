c541743903b7a0ad4d51ff16c50ea2633b9d5f10
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-03-03 14:46:08 +0100

    Populate Hybrid Models.py with exercises

--- Time Series/Hybrid Models.py
+++ Time Series/Hybrid Models.py
@@ -1,3 +1,87 @@
 # ================================================================
 #  Kaggle - Time Series: Hybrid Models
 # ================================================================
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+
+
+# Q1
+# ----------------------------------------------------------------
+def fit(self, X_1, X_2, y):
+    # Train model_1
+    self.model_1.fit(X_1, y)
+
+    # Make predictions
+    y_fit = pd.DataFrame(
+        self.model_1.predict(X_1), 
+        index=X_1.index, columns=y.columns,
+    )
+
+    # Compute residuals
+    y_resid = y - y_fit
+    y_resid = y_resid.stack().squeeze() # wide to long
+
+    # Train model_2 on residuals
+    self.model_2.fit(X_2, y_resid)
+
+    # Save column names for predict method
+    self.y_columns = y.columns
+    # Save data for question checking
+    self.y_fit = y_fit
+    self.y_resid = y_resid
+
+
+# Add method to class
+BoostedHybrid.fit = fit
+
+# Check your answer
+q_1.check()
+
+
+# Q2
+# ----------------------------------------------------------------
+def predict(self, X_1, X_2):
+    # Predict with model_1
+    y_pred = pd.DataFrame(
+        self.model_1.predict(X_1), 
+        index=X_1.index, columns=self.y_columns,
+    )
+    y_pred = y_pred.stack().squeeze()  # wide to long
+
+    # Add model_2 predictions to model_1 predictions
+    y_pred += self.model_2.predict(X_2)
+
+    return y_pred.unstack()
+
+
+# Add method to class
+BoostedHybrid.predict = predict
+
+
+# Check your answer
+q_2.check()
+
+
+# Q3
+# ----------------------------------------------------------------
+# Create model
+model = BoostedHybrid(
+    model_1=LinearRegression(),
+    model_2=XGBRegressor(),
+)
+model.fit(X_1, X_2, y)
+
+y_pred = model.predict(X_1, X_2)
+y_pred = y_pred.clip(0.0)
+
+
+# Check your answer
+q_3.check()
+
+
+# Q4
+# ----------------------------------------------------------------
+# View the solution (Run this cell to receive credit!)
+q_4.check()

881a80045164fdb30f3c58f2a42961feca05840a
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-03-04 08:16:56 +0100

    Populate Partial Plots.py with exercises

--- Machine Learning Explainability/Partial Plots.py
+++ Machine Learning Explainability/Partial Plots.py
@@ -1,3 +1,131 @@
 # ================================================================
 #  Kaggle - Machine Learning Explainability: Partial Plots
 # ================================================================
+
+import pandas as pd
+import numpy as np
+from numpy.random import rand
+from matplotlib import pyplot as plt
+from sklearn.inspection import PartialDependenceDisplay
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import eli5
+from eli5.sklearn import PermutationImportance
+
+
+# Q1
+# ----------------------------------------------------------------
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+for feat_name in base_features:
+    PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+    plt.show()
+
+
+# Q2
+# ----------------------------------------------------------------
+fig, ax = plt.subplots(figsize=(8, 6))
+fnames = [('pickup_longitude', 'dropoff_longitude')]
+disp = PartialDependenceDisplay.from_estimator(first_model, val_X, fnames, ax=ax)
+plt.show()
+
+
+# Q3
+# ----------------------------------------------------------------
+savings_from_shorter_trip = 6
+
+# Check your answer
+q_3.check()
+
+
+# Q4
+# ----------------------------------------------------------------
+# This is the PDP for pickup_longitude without the absolute difference features. Included here to help compare it to the new PDP you create
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+# Your code here
+# create new features
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+feat_name = 'pickup_longitude'
+disp = PartialDependenceDisplay.from_estimator(second_model, new_val_X, [feat_name])
+plt.show()
+
+# Check your answer
+q_4.check()
+
+
+# Q5
+# ----------------------------------------------------------------
+# Check your answer (Run this code cell to receive credit!)
+q_5.solution()
+
+
+# Q6
+# ----------------------------------------------------------------
+n_samples = 20000
+
+# Create array holding predictive feature
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+
+# Your code here
+# Create y. you should have X1 and X2 in the expression for y
+y = -2 * X1 * (X1<-1) + X1 - 2 * X1 * (X1>1) - X2
+
+# create dataframe 
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'])
+plt.show()
+
+# Check your answer
+q_6.check()
+
+
+# Q7
+# ----------------------------------------------------------------
+n_samples = 20000
+
+# Create array holding predictive feature
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+# Create y. you should have X in the expression for y
+y = X1 * X2
+
+
+# create dataframe because pdp_isolate expects a dataFrame as an argument
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+
+
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'], grid_resolution=300)
+plt.show()
+
+perm = PermutationImportance(my_model).fit(predictors_df, my_df.y)
+
+# Check your answer
+q_7.check()
+
+# show the weights for the permutation importance you just calculated
+eli5.show_weights(perm, feature_names = ['X1', 'X2'])

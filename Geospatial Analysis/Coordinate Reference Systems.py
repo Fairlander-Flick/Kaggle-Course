4bb12be4c4d035ca5bb11650740314c422ad89e1
flick <flick@example.com>
2026-03-05 14:09:14 +0100

    Complete Coordinate Reference Systems exercise

--- Geospatial Analysis/Coordinate Reference Systems.py
+++ Geospatial Analysis/Coordinate Reference Systems.py
@@ -1,3 +1,103 @@
 # ================================================================
 #  Kaggle - Geospatial Analysis: Coordinate Reference Systems
 # ================================================================
+
+# Q1
+# Load the data and print the first 5 rows
+birds_df = pd.read_csv("../input/geospatial-learn-course-data/purple_martin.csv", parse_dates=['timestamp'])
+print("There are {} different birds in the dataset.".format(birds_df["tag-local-identifier"].nunique()))
+birds_df.head()
+
+# Create the GeoDataFrame
+birds = gpd.GeoDataFrame(birds_df, geometry=gpd.points_from_xy(birds_df["location-long"], birds_df["location-lat"]))
+
+# Set the CRS to {'init': 'epsg:4326'}
+birds.crs = {'init' :'epsg:4326'}
+# Check your answer
+q_1.check()
+
+# Q2
+# Load a GeoDataFrame with country boundaries in North/South America, print the first 5 rows
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+americas = world.loc[world['continent'].isin(['North America', 'South America'])]
+americas.head()
+
+# Your code here
+ax = americas.plot(figsize=(10,10), color='white', linestyle=':', edgecolor='gray')
+birds.plot(ax=ax, markersize=10)
+
+# Uncomment to zoom in
+ax.set_xlim([-110, -30])
+ax.set_ylim([-30, 60])
+# Uncomment to see a hint
+#q_2.hint()
+
+# Q3
+# GeoDataFrame showing path for each bird
+path_df = birds.groupby("tag-local-identifier")['geometry'].apply(list).apply(lambda x: LineString(x)).reset_index()
+path_gdf = gpd.GeoDataFrame(path_df, geometry=path_df.geometry)
+path_gdf.crs = {'init' :'epsg:4326'}
+
+# GeoDataFrame showing starting point for each bird
+start_df = birds.groupby("tag-local-identifier")['geometry'].apply(list).apply(lambda x: x[0]).reset_index()
+start_gdf = gpd.GeoDataFrame(start_df, geometry=start_df.geometry)
+start_gdf.crs = {'init' :'epsg:4326'}
+
+# Show first five rows of GeoDataFrame
+start_gdf.head()
+
+# Your code here
+end_df = birds.groupby("tag-local-identifier")['geometry'].apply(list).apply(lambda x: x[-1]).reset_index()
+end_gdf = gpd.GeoDataFrame(end_df, geometry=end_df.geometry)
+end_gdf.crs = {'init': 'epsg:4326'}
+
+# Check your answer
+q_3.check()
+
+# Q4
+# Your code here
+ax = americas.plot(figsize=(10, 10), color='white', linestyle=':', edgecolor='gray')
+
+start_gdf.plot(ax=ax, color='red',  markersize=30)
+path_gdf.plot(ax=ax, cmap='tab20b', linestyle='-', linewidth=1, zorder=1)
+end_gdf.plot(ax=ax, color='black', markersize=30)
+
+# Uncomment to see a hint
+#q_4.hint()
+
+# Q5
+# Path of the shapefile to load
+protected_filepath = "../input/geospatial-learn-course-data/SAPA_Aug2019-shapefile/SAPA_Aug2019-shapefile/SAPA_Aug2019-shapefile-polygons.shp"
+
+# Your code here
+protected_areas = gpd.read_file(protected_filepath)
+
+# Check your answer
+q_5.check()
+
+# Q6
+# Country boundaries in South America
+south_america = americas.loc[americas['continent']=='South America']
+
+# Your code here: plot protected areas in South America
+# Plot protected areas in South America
+ax = south_america.plot(figsize=(10,10), color='white', edgecolor='gray')
+protected_areas.plot(ax=ax, alpha=0.4)
+# Uncomment to see a hint
+#q_6.hint()
+
+# Q7
+# Your code here: Calculate the total area of South America (in square kilometers)
+totalArea = sum(south_america.geometry.to_crs(epsg=3035).area) / 10**6
+
+# Check your answer
+q_7.check()
+
+# Q8
+# Your code here
+ax = south_america.plot(figsize=(10,10), color='white', edgecolor='gray')
+protected_areas[protected_areas['MARINE']!='2'].plot(ax=ax, alpha=0.4, zorder=1)
+birds[birds.geometry.y < 0].plot(ax=ax, color='red', alpha=0.6, markersize=10, zorder=2)
+
+# Uncomment to see a hint
+#q_8.hint()

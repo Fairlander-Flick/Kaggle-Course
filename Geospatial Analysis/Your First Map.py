8075091d4ee3b83b18edc2bc01a6aed78c02cd14
flick <flick@example.com>
2026-03-05 12:57:35 +0100

    Complete Your First Map exercise

--- Geospatial Analysis/Your First Map.py
+++ Geospatial Analysis/Your First Map.py
@@ -1,3 +1,33 @@
 # ================================================================
 #  Kaggle - Geospatial Analysis: Your First Map
 # ================================================================
+
+# Q1
+loans_filepath = "../input/geospatial-learn-course-data/kiva_loans/kiva_loans/kiva_loans.shp"
+
+# Your code here: Load the data
+world_loans = gpd.read_file(loans_filepath)
+
+# Check your answer
+q_1.check()
+
+# Uncomment to view the first five rows of the data
+#world_loans.head()
+
+# Q2
+ax = world.plot(figsize=(20,20), color='whitesmoke', linestyle=':', edgecolor='black')
+world_loans.plot(ax=ax, markersize=2)
+
+# Q3
+# Load a KML file containing island boundaries
+gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+PHL = gpd.read_file("../input/geospatial-learn-course-data/Philippines_AL258.kml", driver='KML')
+PHL.head()
+
+# Q4
+# Your code here
+ax = PHL.plot(figsize=(12,12), color='whitesmoke', linestyle=':', edgecolor='lightgray')
+PHL_loans.plot(ax=ax, markersize=2)
+
+# Uncomment to see a hint
+#q_4.a.hint()

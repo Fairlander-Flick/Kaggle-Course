95eb30f889f8010cc31e57af614f58458667029d
flick <flick@example.com>
2026-03-06 22:27:04 +0100

    Complete Deep Reinforcement Learning exercise

--- Intro to Game AI and Reinforcement Learning/Deep Reinforcement Learning.py
+++ Intro to Game AI and Reinforcement Learning/Deep Reinforcement Learning.py
@@ -1,3 +1,14 @@
 # ================================================================
 #  Kaggle - Intro to Game AI and Reinforcement Learning: Deep Reinforcement Learning
 # ================================================================
+
+# Q1
+# Fill in the blank
+best_option = "C"
+
+# Check your answer
+q_1.check()
+
+# Q2
+# Check your answer (Run this code cell to receive credit!)
+q_2.solution()
